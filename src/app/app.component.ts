33fc11d5d354b28f8aff03fbf7f06f56c4fae72f
ianotieno <ianotieno23@gmail.com>
2023-08-29 23:44:10 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,30 +13,7 @@ export class AppComponent {
   profileForm=this.formBuilder.group({
   ReferringDoctor:['',Validators.required],
   Diagnosis:['',Validators.required],
-  Age:['',Validators.required],
-  dob:['',Validators.required],
-  gender:['male',Validators.required],
-  AdminNo:['',Validators.required],
-  NameOfC:['',Validators.required],
-  Admission:['',Validators.required],
-  Comorbidities:['',Validators.required],
-  
-  History:['',Validators.required],
-  Investigations:['',Validators.required],
-  Labs:['',Validators.required],
-  XRays:['',Validators.required],
-  Management:['',Validators.required],
-  Rom:['',Validators.required],
-  Gait:['',Validators.required],
-  Mobility:['',Validators.required],
-  Motor:['',Validators.required],
-  Occupational:['',Validators.required],
-  Speech:['',Validators.required],
-  Nursing:['',Validators.required],
-  Clinical:['',Validators.required],
-  Medical:['',Validators.required],
-  Instruction:['',Validators.required],
-  Doctor:['',Validators.required]
+  chiefcomplians:['',Validators.required],
 
   });
   saveForm(){
